6055ff159ccc819a165f1b5701978fbc554be8f9
Harsh Sahu <harshsahu1109@gmail.com>
2026-03-10 20:02:55 +0530

    Frontend Backend Connections

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     await handleRegister({ username, email, password });
-    navigate("/login");
+    navigate("/");
   };
 
   if (loading) {
