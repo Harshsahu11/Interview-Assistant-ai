bbf15dc4185485bd0fa5e5adb0f92fe7631975e5
Harsh Sahu <harshsahu1109@gmail.com>
2026-03-11 19:28:10 +0530

    Backend Interview Reports generation

--- Backend/src/models/interviewReport.model.js
+++ Backend/src/models/interviewReport.model.js
@@ -104,6 +104,10 @@ const interviewReportSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "user",
     },
+    title:{
+      type:String,
+      required:[true,"Job title is required"]
+    }
   },
   {
     timestamps: true,
--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -1,10 +1,16 @@
 const express= require('express');
 const interviewRouter = express.Router();
 const authUser = require("../middlewares/auth.middleware");
-const {generateInterViewReportController} = require('../controllers/interview.controller');
+const {generateInterViewReportController,getInterviewReportByIdController,getAllInterviewReport} = require('../controllers/interview.controller');
 const upload = require('../middlewares/file.middleware');
 
 
 interviewRouter.post("/",authUser,upload.single("resume"),generateInterViewReportController);
+
+
+interviewRouter.get("/report/:interviewId",authUser,getInterviewReportByIdController);
+
+
+interviewRouter.get("/",authUser,getAllInterviewReport)
 
 module.exports = interviewRouter;
